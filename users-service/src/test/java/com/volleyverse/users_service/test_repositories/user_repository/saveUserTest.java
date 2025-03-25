0d4131956af11aa95d347fa149829629cda0d76f
ricardo537 <dawricardo735@gmail.com>
2025-03-25 16:59:51 +0100

    Test de guardar y buscar usuarios documentados en JavaDoc

--- users-service/src/test/java/com/volleyverse/users_service/test_repositories/user_repository/saveUserTest.java
+++ users-service/src/test/java/com/volleyverse/users_service/test_repositories/user_repository/saveUserTest.java
@@ -8,18 +8,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
 
+/**
+ * The tests will be name with this structure: nameMethod_functionalitySuccess or nameMethod_functionalitySuccess¿Why?
+ * This class will verify that the methods of saving users will work in the following situations:
+ */
 @DataJpaTest
 class saveUserTest {
 	
     @Autowired
     private UserRepository userRepository;
 
+    /**
+     * Before all test we make sure the user John Doe doesn't exists in the data base
+     */
     @BeforeEach
     void setUp() {
         // Borra solo el usuario con email "johndoe@example.com" si existe
         userRepository.deleteByEmail("johndoe@example.com");  
     }
 
+    /**
+     * This test will save the user of John Doe, it has to save the user.
+     */
     @Test
     void save_savedSuccess() {
         User user = new User(null, "johndoe@example.com", "JohnDoe1!", "John Doe");
@@ -31,6 +41,9 @@ class saveUserTest {
         assertEquals("John Doe", savedUser.getName());
     }
     
+    /**
+     * This test will update the user name of John Doe to Sam Las, the user's name doesn't have restriction so it can be repeated, it has to update the user.
+     */
     @Test
     void save_updatedSuccess() {
         User user = new User(null, "johndoe@example.com", "JohnDoe1!", "John Doe");
@@ -43,6 +56,9 @@ class saveUserTest {
         assertEquals("Sam Las", updateUser.getName());
     }
 
+    /**
+     * This test will try to update an user email to an email that already exists in the database, how email has to be unique it wouldn't work and will throw an exception.
+     */
     @Test
     void save_updatedFailureEmailAlreadyExists() {
         User user1 = new User(null, "johndoe@example.com", "JohnDoe1!", "John Doe");
@@ -54,9 +70,12 @@ class saveUserTest {
 
         assertThrows(DataIntegrityViolationException.class, () -> userRepository.save(user1));
     }
-
+    
+    /**
+     * This test will try to save a new user with an email that is already register in the database, how the email has to be unique it wouldn't work and will throw an exception.
+     */
     @Test
-    void save_notSavedEmailAlreadyExists() {
+    void save_savedFailureEmailAlreadyExists() {
         User user1 = new User(null, "johndoe@example.com", "JohnDoe1!", "John Doe");
         userRepository.save(user1);
 
--- users-service/src/test/java/com/volleyverse/users_service/test_repositories/user_repository/searchUserTest.java
+++ users-service/src/test/java/com/volleyverse/users_service/test_repositories/user_repository/searchUserTest.java
@@ -9,6 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+/**
+ * The tests will be name with this structure: nameMethod_functionalitySuccess or nameMethod_functionalitySuccess¿Why?
+ * This class will verify that the methods of searching the user via email or email and password will work in the following situations:
+ */
 @DataJpaTest
 class searchUserTest {
 	
@@ -17,12 +21,18 @@ class searchUserTest {
 	
 	private User user;
 	
+	/**
+	 * Before all tests, we will register in the database a default user "John Doe" because for searching a user we need to have users in the database.
+	 */
 	@BeforeEach
 	void setUp() {
 		this.user = new User(null, "johndoe@example.com", "JohnDoe1!", "John Doe");//Cuando se haya creado el modelo se creará una instancia con los datos de johndoe@example.com JohnDoe1! John Doe
 		userRepository.save(this.user);
 	}
 
+	/**
+	 * This test will search the user of John Doe via email, it has to found.
+	 */
 	@Test
 	void findByEmail_foundSuccess() {
 		Optional<User> userFound = this.userRepository.findByEmail("johndoe@example.com");
@@ -31,13 +41,19 @@ class searchUserTest {
 		assertEquals("John Doe", userFound.get().getName());
 	}
 	
+	/**
+	 * This test will search user via email that it's not register in the database, so it can found anything.
+	 */
 	@Test
-	void findByEmail_notFoundSuccess() {
+	void findByEmail_foundFailure() {
 		Optional<User> userFound = this.userRepository.findByEmail("samlas@example.com");
 		
 		assertFalse(userFound.isPresent());
 	}
 	
+	/**
+	 * This test will search the user of John Doe via email and password, it has to found.
+	 */
 	@Test
 	void findByEmailAndPassword_foundSuccess() {
 		Optional<User> userFound = this.userRepository.findByEmailAndPassword("johndoe@example.com", "JohnDoe1!");
@@ -46,8 +62,11 @@ class searchUserTest {
 		assertEquals("John Doe", userFound.get().getName());
 	}
 	
+	/**
+	 * This test will search user via email and password that email was registered but not with the same password we register the user in the database, so it can found anything.
+	 */
 	@Test
-	void findByEmailAndPassword_notFoundSuccess() {
+	void findByEmailAndPassword_foundFailure() {
 		Optional<User> userFound = this.userRepository.findByEmailAndPassword("johndoe@example.com", "SamLas1!");
 		
 		assertFalse(userFound.isPresent());
