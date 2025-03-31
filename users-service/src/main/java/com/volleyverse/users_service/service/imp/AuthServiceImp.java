b2c6823778e5c14ec9b54489cab6713d16ba29e6
ricardo537 <dawricardo735@gmail.com>
2025-03-31 14:07:32 +0200

    Implementación de JavaDoc en el AuthService

--- users-service/src/main/java/com/volleyverse/users_service/service/imp/AuthServiceImp.java
+++ users-service/src/main/java/com/volleyverse/users_service/service/imp/AuthServiceImp.java
@@ -11,18 +11,27 @@ import com.volleyverse.users_service.entity.User;
 import com.volleyverse.users_service.repository.UserRepository;
 import com.volleyverse.users_service.service.AuthService;
 
+/**
+ * This class will manage the CRUD of the user's accounts, it's responsible of transform or validate the requests data, so that in the end the repository interacts with the database when if necessary.
+ */
 @Service
 public class AuthServiceImp implements AuthService {
 	
+	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
-	public AuthServiceImp(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-		this.userRepository = userRepository;
-		this.passwordEncoder = passwordEncoder;
-	}
 
+	/**
+	 * This method will validate the structure of the email and password. If all the data is valid the method will search for another user with that email, cause the email can be repeated.
+	 * If all there isn't another user with that email, this will encode the password and save in the database.
+	 * @section Email Validation:
+	 * The email has to have the next structure: name@mail.domain.
+	 * The name is the name of the email that user introduce when the user create it.
+	 * The mail is the type of mail, for example: "gmail", "yahoo" or "hotmail".
+	 * The domain is the last part of the email, it can be "com" or "es".
+	 */
 	public String register(UserRegisterRequest userRegisterRequest) {
 		String emailRegex = "^[a-zA-Z0-9._]+@[a-zA-Z0.-]+\\.[a-zA-Z]{2,}$";
 		
@@ -53,6 +62,16 @@ public class AuthServiceImp implements AuthService {
 		return "El usuario ha sido registrado con éxito.";
 	}
 	
+	/**
+	 * This method validate that the password. In case the password is correct, this will return no message (""), in the other case this will return an error message with the criterion that does not meet.
+	 * @section Password Validation:
+	 * - The password has to have minimum 7 characters.
+	 * - The password has to have minimum one number.
+	 * - The password has to have minimum one capital and one lower case 
+	 * - The password has to have minimum one special character
+	 * @param password
+	 * @return errorMessage
+	 */
 	private String getErrorPasswordMessage(String password) {
 	    if (password.length() < 7) {
 	        return "La contraseña tiene que tener mínimo 7 caracteres.";
